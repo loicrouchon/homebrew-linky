c31520d4468a934ae5348ae748d4a16d1c48af9c
jreleaserbot <loicrouchon+jreleaser@gmail.com>
2021-11-25 20:11:00 +0000

    symly v0.4.3

--- Formula/symly.rb
+++ Formula/symly.rb
@@ -1,9 +1,9 @@
 class Symly < Formula
   desc "Symbolic link management tool"
   homepage "https://github.com/loicrouchon/symly"
-  version "0.4.1"
-  url "https://github.com/loicrouchon/symly/releases/download/v0.4.1/symly-0.4.1-homebrew-bottle.zip"
-  sha256 "c0e87606b188ba08f5aff88fc0c4203b5a4b79772cd7976a201d71cff8d3a193"
+  version "0.4.3"
+  url "https://github.com/loicrouchon/symly/releases/download/v0.4.3/symly-0.4.3-homebrew-bottle.zip"
+  sha256 "469183da5f04fffc8fc655ce822f269e4f1abada89684c3ca613e4cbd5fcb822"
   license "Apache-2"
 
   depends_on "openjdk"
@@ -16,6 +16,6 @@ class Symly < Formula
 
   test do
     output = shell_output("#{bin}/symly --version")
-    assert_match "0.4.1", output
+    assert_match "0.4.3", output
   end
 end

08061db209be6d74ca4383e20e4da00ab6fbf8b2
jreleaserbot <loicrouchon+jreleaser@gmail.com>
2021-11-28 12:25:09 +0000

    homebrew v0.4.5

--- Formula/symly.rb
+++ Formula/symly.rb
@@ -1,21 +1,21 @@
 class Symly < Formula
   desc "Symbolic link management tool"
   homepage "https://github.com/loicrouchon/symly"
-  version "0.4.4"
-  url "https://github.com/loicrouchon/symly/releases/download/v0.4.4/symly-0.4.4-homebrew-bottle.zip"
-  sha256 "65af55e355d5b91fff2ee478a7f36e192bde98ede6deeb42d4cd1baae71ad2d2"
+  url "https://github.com/loicrouchon/symly/releases/download/v0.4.5/symly-0.4.5-homebrew-bottle.zip"
+  version "0.4.5"
+  sha256 "abe4f9524ebb7e4591d1772c38397fcfce7e17cfdbec2c2a2ae27062121f1c9d"
   license "Apache-2"
 
   depends_on "coreutils"
   depends_on "openjdk"
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/symly"
+    prefix.install Dir["*"]
+    bin.install_symlink "#{bin}/symly"
   end
 
   test do
     output = shell_output("#{bin}/symly --version")
-    assert_match "0.4.4", output
+    assert_match "0.4.5", output
   end
 end

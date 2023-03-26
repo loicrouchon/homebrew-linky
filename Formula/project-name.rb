2cd60917d88a9409ae6645ea7f59be39d824cae3
jreleaserbot <loicrouchon+jreleaser@gmail.com>
2023-03-26 16:02:33 +0000

    homebrew v0.10.0

--- Formula/project-name.rb
+++ Formula/project-name.rb
@@ -1,9 +1,9 @@
 class Symly < Formula
   desc "A tool for managing symbolic links"
   homepage "https://github.com/loicrouchon/symly"
-  url "https://github.com/loicrouchon/symly/releases/download/v0.9.4/symly-0.9.4-homebrew-bottle.zip"
-  version "0.9.4"
-  sha256 "48305f7a9e0ce41fcf705ce2fd04060e6a29e4475ef660de555d1fbdf16e79d0"
+  url "https://github.com/loicrouchon/symly/releases/download/v0.10.0/symly-0.10.0-homebrew-bottle.zip"
+  version "0.10.0"
+  sha256 "de8095fd4de8585d1a9a3db965a9e33e53c65c8bb0b5a2e2ed86b967caa5b0d9"
   license "Apache-2.0"
 
   depends_on "coreutils"
@@ -15,6 +15,6 @@ class Symly < Formula
 
   test do
     output = shell_output("#{bin}/symly --version")
-    assert_match "0.9.4", output
+    assert_match "0.10.0", output
   end
 end

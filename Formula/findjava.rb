2ef703a9de21c8f236de7a9cb479f4d89564cfd7
Loic Rouchon <loic@loicrouchon.com>
2023-06-25 17:09:57 +0200

    Fix brew formulas' style

--- Formula/findjava.rb
+++ Formula/findjava.rb
@@ -1,22 +1,22 @@
 class Findjava < Formula
   VERSION = "0.3.2".freeze
 
-  desc "Finds the best JVM to run your java program "
+  desc "Finds the best JVM to run your java program"
   homepage "https://github.com/loicrouchon/findjava"
   url "https://github.com/loicrouchon/findjava/archive/v#{VERSION}.tar.gz"
   sha256 "066a91ea298ed49037bc7306d59224bcf82eded72c38af79598bab49f97484df"
   license "Apache-2.0"
 
-  depends_on "make" => :build
   depends_on "golang" => :build
+  depends_on "make" => :build
   depends_on "openjdk" => :build
 
   def install
     ENV["GO_LD_FLAGS"] = "-linkmode=external"
     if OS.mac?
-        ENV["GO_TAGS"] = "-tags darwin"
+      ENV["GO_TAGS"] = "-tags darwin"
     elsif OS.linux?
-        ENV["GO_TAGS"] = "-tags linux"
+      ENV["GO_TAGS"] = "-tags linux"
     end
     system "make", "clean", "test", "build"
     bin.install_symlink libexec/"findjava"
--- Formula/symly.rb
+++ Formula/symly.rb
@@ -2,7 +2,6 @@ class Symly < Formula
   desc "Utility for centralizing sparse file trees with symbolic links"
   homepage "https://github.com/loicrouchon/symly"
   url "https://github.com/loicrouchon/symly/releases/download/v0.10.4/symly-0.10.4-homebrew-bottle.zip"
-  version "0.10.4"
   sha256 "126c67e73f00ce46c3f6f8a0a3748d8eadc1f2468a5de1f4c771b7ceea3acd60"
   license "Apache-2.0"
 

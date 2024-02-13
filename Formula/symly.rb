f47e97103b4ec3ea261b7536e37397fd36d734c3
jreleaserbot <loicrouchon+jreleaser@gmail.com>
2024-02-13 08:47:56 +0000

    homebrew v0.11.7

--- Formula/symly.rb
+++ Formula/symly.rb
@@ -1,8 +1,8 @@
 class Symly < Formula
   desc "Utility for centralizing sparse file trees with symbolic links"
   homepage "https://github.com/loicrouchon/symly"
-  url "https://github.com/loicrouchon/symly/releases/download/v0.11.6/symly-0.11.6-homebrew.zip"
-  sha256 "fe9cf5997af0efcd2029d6cca053a5b11294d21c67f35e2056185d25cf658749"
+  url "https://github.com/loicrouchon/symly/releases/download/v0.11.7/symly-0.11.7-homebrew.zip"
+  sha256 "3514cc4cb4a820156529d90f086988c44a6f6f4b6277b899070a571f66b66985"
   license "Apache-2.0"
 
   depends_on "coreutils"
@@ -14,6 +14,6 @@ class Symly < Formula
 
   test do
     output = shell_output("#{bin}/symly --version")
-    assert_match "0.11.6", output
+    assert_match "0.11.7", output
   end
 end
